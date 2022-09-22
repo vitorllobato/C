718c08178c469b6609369d39a1048d8ea1d551be
vitorllobato <vitor.vasconcelos@ufv.br>
2022-09-22 14:48:38 -0300

    Lista repetição completa.

--- C/CCF110_C/Lista2_Repetição/exercicio13.c
+++ C/CCF110_C/Lista2_Repetição/exercicio13.c
@@ -7,9 +7,10 @@ int main(){
     printf("Digite um valor:");
     scanf("%d",&n);
     int i=0;
-    for (i ; i<=n; i++){
+    while (i<=n){
         E=i*n;
         printf("%d*%d = %d\n",i,n,E);
+        i++;
 
     }
 
