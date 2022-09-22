13d32e6bfba2034c38e763ad080fd366ee60b7c5
vitorllobato <vitor.vasconcelos@ufv.br>
2022-09-21 21:11:12 -0300

    Exercicio 1 concluído

--- C/CCF110_C/Lista2_Repetição/exercicio1.c
+++ C/CCF110_C/Lista2_Repetição/exercicio1.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+float n;
+int quantidade = 3;
+
+int main(){
+    for (int i = 0; i<quantidade; i++){
+        printf("Digite um numero: ");
+        scanf("%f", &n);
+        printf("%f\n", n*n*n);
+    }
+
+
+    return 0;
+}
