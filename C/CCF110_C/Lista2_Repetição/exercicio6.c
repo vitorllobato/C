eda6af98a7de608c4259f512c648bb9fcd77711c
vitorllobato <vitor.vasconcelos@ufv.br>
2022-09-22 11:00:19 -0300

    exercicio6 agora está funcionando.

--- C/CCF110_C/Lista2_Repetição/exercicio6.c
+++ C/CCF110_C/Lista2_Repetição/exercicio6.c
@@ -6,13 +6,13 @@ unsigned int n,i,fat;
 int main(){
     printf("Digite um numero para calcular seu fatorial: ");
     scanf("%u",&n);
-    fat = n;
-    for (n; n > 1; i--)
+    fat=n;
+    for (n; n > 1; n--)
     {
-        fat=fat*n-1;
+        fat=fat*(n-1);
+        
     }
-    printf("%u",fat);
-
+    printf("%u\n",fat);
 
     return 0;
 }
