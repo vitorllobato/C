f3ecc9930e7cccba2ced1090d5e06218cd540a32
vitorllobato <vitor.vasconcelos@ufv.br>
2022-09-26 10:22:15 -0300

    Realocação da lista do slide 2.

---
+++ C/Listas Slides/Reptições/Ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiplos,i;
+
+int main(){
+    for ( i = 0; multiplos < 500; i++){
+        multiplos=i*5;
+        printf("%d\n",multiplos);    
+        }
+    return 0;
+}
