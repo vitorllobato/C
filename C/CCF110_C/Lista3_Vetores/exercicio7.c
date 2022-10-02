dcb1603aac94e087abeba6ac7e4038546f7edf68
Vitor Vasconcelos Lobato <vitor.vasconcelos@ufv.br>
2022-10-02 20:36:03 -0300

    Exercicio 7 está pronto.

---
+++ C/CCF110_C/Lista3_Vetores/exercicio7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+                    int n=0,vetor1[10],vetor2[10],vetorResultante[20],i;
+    for (i=0; i < 10; i++){
+        printf("Digite um numero do vetor 1: ");
+        scanf("%d",&vetor1[i]);
+        }
+    for (i=0; i < 10; i++){
+        printf("Digite um numero do vetor 2: ");
+        scanf("%d",&vetor2[i]);
+        }
+    for (i=0; i < 20; i++){
+        vetorResultante[i]=vetor1[i];
+        vetorResultante[i+1]=vetor2[i];
+        i++;
+        }
+    printf("Vetor resultante: ");
+    for (i=0; i < 20; i++){
+        printf("%d,",vetorResultante[i]);
+        }  
+
+    return 0;
+}
