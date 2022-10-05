e36ecf383e84c8c4d2a1f325f1b698bbbafea091
Vitor Vasconcelos Lobato <vitor.vasconcelos@ufv.br>
2022-10-05 10:07:26 -0300

    Fazendo correção do exercicio 8.

--- C/CCF110_C/Lista3_Vetores/exercicio8.c
+++ C/CCF110_C/Lista3_Vetores/exercicio8.c
@@ -8,11 +8,21 @@ int main(){
     for(int i=0;i<n;i++){
         printf("Digite o valor do vetorA[%d]: ",i);
         scanf("%d",&vetorA[i]);
+        }
+    for(int i=0;i<n;i++){
         printf("Digite o valor do vetorB[%d]: ",i);
         scanf("%d",&vetorB[i]);
-        vetorC[i]=vetorA[i]+vetorB[i];
-        vetorD[i]=vetorA[i]*vetorB[i];
-    }
+        }
+    for (int i = 0; i < n; i++){
+        vetorC[i]=(vetorA[i]+vetorB[i])/2;
+        printf("%d ",vetorC[i]);
+        }
+    for (int i = 0; i < n; i++){
+        vetorC[i]=(vetorA[i]+vetorB[i])/2;
+        printf("%d ",vetorC[i]);
+        }
+     
+
 
     return 0;
 }
