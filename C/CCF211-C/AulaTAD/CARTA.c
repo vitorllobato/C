0b69338561e709217d046635c0f56fc906ef595a
Vitor Vasconcelos Lobato <95286115+vitorllobato@users.noreply.github.com>
2023-03-30 11:10:09 -0300

    Primeira TAD
    Primeira TAD com muitos erros e não funcionando

---
+++ C/CCF211-C/AulaTAD/CARTA.c
@@ -0,0 +1,10 @@
+#include "CARTA.h"
+
+
+
+void random(){
+    srand(time(NULL));
+    int CARTA.numcarta = rand()%13;
+    int CARTA.naipe = rand()%4;
+
+    }
---
+++ C/CCF211-C/AulaTAD/CARTA.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#ifndef TAD_CARTA_H
+#define TAD_CARTA_H
+
+typedef struct CARTA{
+    int numcarta;
+    int naipe;
+}CARTA;
+
+
+
+
+
+#endif /* TAD_CARTA_H */
